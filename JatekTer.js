ff83cedc5fce12f1e945d5e38c8e6e6975da707a
VeryCreativeName64 <marekbendeguz@gmail.com>
2025-05-22 12:04:13 +0200

    visszafordit

--- JatekTer.js
+++ JatekTer.js
@@ -3,9 +3,10 @@ import Kartya from "./Kartya.js";
 export default class JatekTer {
   #kartyaLista = [];
   #szElem = document.querySelector(".jatekter");
-  #kivalasztottKartyaLista = [];
+  #kivalasztottKartyaLista = []
   constructor(kartyaLista) {
     this.#kartyaLista = kartyaLista;
+    
     this.#init();
   }
   #init() {
@@ -13,10 +14,33 @@ export default class JatekTer {
     this.#kartyaLista.forEach((element, index) => {
       const kartya = new Kartya(index, element, this.#szElem);
     });
+    this.#ellenorzes();
   }
-  #kever(){
-    this.#kartyaLista.sort((a,b)=>{
-        return Math.random()-0.5
-    })
+  #kever() {
+    this.#kartyaLista.sort((a, b) => {
+      return Math.random() - 0.5;
+    });
+  }
+  #ellenorzes() {
+    window.addEventListener("fordit", (event) => {
+      if (this.#kivalasztottKartyaLista.length < 2) {
+        this.#kivalasztottKartyaLista.push(event.detail);
+      }
+      console.log(this.#kivalasztottKartyaLista);
+      if (this.#kivalasztottKartyaLista.length === 2) {
+        let f1 = this.#kivalasztottKartyaLista[0];
+        let f2 = this.#kivalasztottKartyaLista[1];
+        if (f1.getFajlNev() == f2.getFajlNev()) {
+          console.log("pár");
+        } else {
+          console.log("nem pár");
+          setTimeout(() => {
+            f1.setAllapot();
+            f2.setAllapot();
+          }, 2000);
+        }
+        this.#kivalasztottKartyaLista.splice(0);
+      }
+    });
   }
 }
--- Kartya.js
+++ Kartya.js
@@ -8,16 +8,44 @@ export default class Kartya{
         this.#id=id;
         this.#fajlnev=fajlnev;
         this.#divElem=szuloElem;
-        this.megjelenit();
+        this.#megjelenit();
+        this.#kattintasTrigger();    
     }
-    megjelenit(){
+    #megjelenit(){
         /* egy kártya megjelenítése */
         let html = ` 
         <div class="kartya">
-            <img src="${this.#fajlnev}" alt="kép">
+            <img src="kepek/hatter.jpg" alt="kép">
         </div>
         `
         
         this.#divElem.insertAdjacentHTML("beforeend",html);
+    }
+    setAllapot(){
+        this.#allapot= !this.#allapot;
+        this.setLap()
+    }
+    setLap(){
+        if(this.#allapot){
+            this.#imgElem.src=this.#fajlnev
+        }else{
+            this.#imgElem.src = "kepek/hatter.jpg"
+        }
+        
+    }
+
+    getFajlNev(){
+        return this.#fajlnev
+    }
+
+    #kattintasTrigger(){
+        this.#imgElem=document.querySelector(".kartya:last-child img")
+        this.#imgElem.addEventListener("click",()=>{
+            console.log(this.#imgElem)
+            const e=new CustomEvent("fordit",{detail:this})
+            window.dispatchEvent(e)
+            this.setAllapot()
+        })
+        
     }
 }
